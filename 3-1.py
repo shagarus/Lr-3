41cc44bfc4e4dd94419ebb0bc092272fb36814b0
shagarus <ukughjh@mail.ru>
2025-04-21 00:42:37 +0500

    Add files via upload
    Перенес функционал в контейнер

--- 3-1.py
+++ 3-1.py
@@ -6,15 +6,52 @@ from PyQt6.QtCore import Qt, QRectF
 class Container:
     def __init__(self):
         self.__data = []
+    
     def add(self, obj):
         self.__data.append(obj)
+    
     def remove(self, obj):
         self.__data.remove(obj)
+    
     def get_all(self):
         return self.__data[:]
+    
+    def deselect_all(self):
+        for item in self.__data:
+            item.selected = False
+    
+    def get_at_position(self, x, y):
+        return [item for item in self.__data if item.contains(x, y)]
+    
+    def get_last_at_position(self, x, y):
+        circles = self.get_at_position(x, y)
+        return circles[-1] if circles else None
+    
+    def get_selected(self):
+        return [item for item in self.__data if item.selected]
+    
+    def toggle_selection_at(self, x, y):
+        for item in self.__data:
+            if item.contains(x, y):
+                item.selected = not item.selected
+    
+    def select_at(self, x, y):
+        for item in self.__data:
+            item.selected = item.contains(x, y)
+
+    def toggle_selection_last_at(self, x, y):
+        circle = self.get_last_at_position(x, y)
+        if circle:
+            circle.selected = not circle.selected
+    
+    def select_last_at(self, x, y):
+        self.deselect_all()
+        circle = self.get_last_at_position(x, y)
+        if circle:
+            circle.selected = True
 
 class CCircle:
-    def __init__(self, x, y, radius=20, color=Qt.GlobalColor.white):
+    def __init__(self, x, y, radius=20, color=Qt.GlobalColor.black):
         self.x = x
         self.y = y
         self.radius = radius
@@ -27,17 +64,19 @@ class CCircle:
     def draw(self, painter: QPainter):
         pen = QPen(self.color)
         painter.setPen(pen)
+        pen.setWidth(2)
         if self.selected:
-            painter.setBrush(Qt.GlobalColor.white)
+            painter.setBrush(Qt.GlobalColor.red)
         else:
             painter.setBrush(Qt.GlobalColor.blue)
         painter.drawEllipse(QRectF(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2))
 
-class CCirclewidget(QWidget):
+class CCircleWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setGeometry(600, 600, 600, 600)
         self.storage = Container()
+        self.setWindowTitle("Лабораторная работа 3. Часть 1: Круги на форме")
         self.show()
     
     def paintEvent(self, event):
@@ -50,34 +89,24 @@ class CCirclewidget(QWidget):
             pos = event.position()
             x = pos.x()
             y = pos.y()
-            circle_click = False
-            for circle in (self.storage.get_all()):
-                if circle.contains(x, y):
-                    if event.modifiers() & Qt.KeyboardModifier.ControlModifier:
-                        circle.selected = not circle.selected
-                    else:
-                        for c in self.storage.get_all():
-                            c.selected = False
-                        circle.selected = True
-                    circle_click = True
-                    break
-            if not circle_click:
-                for c in self.storage.get_all():
-                    c.selected = False
-                self.storage.add(CCircle(x, y))
+            if event.modifiers() & Qt.KeyboardModifier.ControlModifier:
+                self.storage.toggle_selection_last_at(x, y)
+            else:
+                if not self.storage.get_at_position(x, y):
+                    self.storage.deselect_all()
+                    self.storage.add(CCircle(x, y))
+                else:
+                    self.storage.select_last_at(x, y)
+            
             self.update()
 
     def keyPressEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key.Key_Backspace:
-            delete = [c for c in self.storage.get_all() if c.selected]
-            for c in delete:
-                self.storage.remove(c)
+            for circle in self.storage.get_selected():
+                self.storage.remove(circle)
             self.update()
-    
-    def resizeEvent(self, a0):
-        self.update()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = CCirclewidget()
+    window = CCircleWidget()
     sys.exit(app.exec())
